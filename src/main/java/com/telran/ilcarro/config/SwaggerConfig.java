4bb8c7343f89808b63f861b8d43b2467bb2d146d
Anton Konkin <konkin.anton@gmail.com>
2019-12-16 12:28:22 +0200

    Swagger configuration added

--- src/main/java/com/telran/ilcarro/config/SwaggerConfig.java
+++ src/main/java/com/telran/ilcarro/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("com.telran.icarro.controller"))
+                        .basePackage("com.telran.ilcarro.controller"))
                 .paths(PathSelectors.regex("/.*"))
                 .build()
                 .apiInfo(apiEndPointsInfo())
@@ -25,8 +26,8 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder().title("Spring ")
-                .description("iCarro projec")
+        return new ApiInfoBuilder().title("iCarro")
+                .description("iCarro project backend by command #A")
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .version("1.0.0")
