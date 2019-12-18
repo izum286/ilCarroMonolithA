e69bd403309b92490b156fffaafca1f6bd9d58a6
Anton Konkin <konkin.anton@gmail.com>
2019-12-18 15:33:34 +0200

    AuthConfig upd

--- src/main/java/com/telran/ilcarro/config/SecurityConfig.java
+++ src/main/java/com/telran/ilcarro/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.telran.ilcarro.config;
 
+import com.telran.ilcarro.repository.UserDetailsRepository;
+import com.telran.ilcarro.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    UserDetailsRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(repository);
+    }
 
     @Bean
     public PasswordEncoder encoder() {
@@ -18,7 +29,7 @@ public class SecurityConfig {
     }
 
     @Configuration
-    class SecurityAdapter extends WebSecurityConfigurerAdapter {
+    static class SecurityAdapter extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
 
