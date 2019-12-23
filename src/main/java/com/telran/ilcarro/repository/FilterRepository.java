faeadaf6b095b0ad73fcef9620b7000d682b43cc
izum <i401623@gmail.com>
2019-12-23 13:25:12 +0200

    Added filter repository class Deleted mock hashTable from filterService

---
+++ src/main/java/com/telran/ilcarro/repository/FilterRepository.java
@@ -0,0 +1,40 @@
+package com.telran.ilcarro.repository;
+
+import com.telran.ilcarro.service.model.FilterNode;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * mock filter repository
+ * @author izum286
+ */
+@Repository
+public class FilterRepository {
+    Hashtable<String, FilterNode> hashArray = new Hashtable<>();
+
+    public int size(){
+        return hashArray.size();
+    }
+
+    public Collection<FilterNode> values(){
+        return hashArray.values();
+    }
+
+    public boolean containsKey(Object key){
+        return hashArray.containsKey(key);
+    }
+
+    public FilterNode put (String key, FilterNode value){
+        return hashArray.put(key, value);
+    }
+
+    public FilterNode get (String key){
+        return hashArray.get(key);
+    }
+
+
+}
