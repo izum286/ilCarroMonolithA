66ffcbcc8fc9ae39c415f9c87061fd277683cd23
AleksGor224 <47452644+AleksGor224@users.noreply.github.com>
2019-12-23 19:31:40 +0200

    Java docs added

--- src/main/java/com/telran/ilcarro/controller/CarController.java
+++ src/main/java/com/telran/ilcarro/controller/CarController.java
@@ -18,18 +18,51 @@ import java.util.List;
 
 public interface CarController {
 
+    /**
+     * Returns the shortCarDTO that was created from FullCarDTO
+     * @param carDTO of new car
+     * @return ShortCarDTO
+     */
     ShortCarDTO addCar(FullCarDTO carDTO);
 
+    /**
+     * Returns the shortCarDTO that was created from FullCarDTO
+     * @param carDTO of updated car with updated params
+     * @return ShortCarDTO
+     */
     ShortCarDTO updateCar(FullCarDTO carDTO);
 
+    /**
+     * Method for deleting an existing car
+     * @param id of car to be deleted
+     */
     void deleteCar(String id);
 
+    /**
+     * Returns the FullCarDTO for regular users
+     * @param id of car
+     * @return FullCarDTO
+     */
     FullCarDTO getCarByIdForUsers(String id);
 
+    /**
+     * Returns list of FullCarDTO for owner
+     * @return List<FullCarDTO>
+     */
     List<FullCarDTO> ownerGetCars();
 
+    /**
+     * Returns the FullCarDTO for car owner by id of his car
+     * @param id of car
+     * @return FullCarDTO
+     */
     FullCarDTO ownerGetCarById(String id);
 
+    /**
+     * Returns list ShedularUsageDTO for owner about his car
+     * @param id of car
+     * @return List<ShedularUsageDTO>
+     */
     List<SchedularUsageDTO>  ownerGetBookedPeriodsByCarId(String id);
 
 }
