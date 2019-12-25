be56880f9280bd680b55a564390e556fa083cb20
izum <i401623@gmail.com>
2019-12-25 19:04:08 +0200

    diffent constructors added for implement different server responses

--- src/main/java/com/telran/ilcarro/model/web/BookedPeriodsDto.java
+++ src/main/java/com/telran/ilcarro/model/web/BookedPeriodsDto.java
@@ -6,6 +6,7 @@ import lombok.*;
 /**
  * @author vitalii_adler
  * @author izum286
+ * diffent constructors added for implement different server responses
  */
 
 @AllArgsConstructor
@@ -23,4 +24,21 @@ public class BookedPeriodsDto {
     private float amount;
     private String booking_date;
     PersonWhoBookedDto personWhoBookedDto;
+
+    public BookedPeriodsDto(String start_date_time, String end_date_time) {
+        this.start_date_time = start_date_time;
+        this.end_date_time = end_date_time;
+    }
+
+    public BookedPeriodsDto(String start_date_time, String end_date_time, PersonWhoBookedDto personWhoBookedDto) {
+        this.start_date_time = start_date_time;
+        this.end_date_time = end_date_time;
+        this.personWhoBookedDto = personWhoBookedDto;
+    }
+
+    public BookedPeriodsDto(String order_id, float amount, String booking_date) {
+        this.order_id = order_id;
+        this.amount = amount;
+        this.booking_date = booking_date;
+    }
 }
