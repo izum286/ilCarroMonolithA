6b36ef8f0868f034dc0afe90828dfa88b953d6a2
Anton Konkin <konkin.anton@gmail.com>
2020-01-07 12:45:20 +0200

    UserService methods upd

--- src/main/java/com/telran/ilcarro/service/user/UserService.java
+++ src/main/java/com/telran/ilcarro/service/user/UserService.java
@@ -1,11 +1,14 @@
 package com.telran.ilcarro.service.user;
 
+import com.telran.ilcarro.model.car.BookedPeriodDto;
 import com.telran.ilcarro.model.user.FullUserDTO;
 import com.telran.ilcarro.model.user.RegUserDTO;
 import com.telran.ilcarro.model.user.UpdUserDTO;
+import com.telran.ilcarro.repository.entity.BookedPeriodEntity;
 import com.telran.ilcarro.service.exceptions.ConflictServiceException;
 import com.telran.ilcarro.service.exceptions.NotFoundServiceException;
 
+import java.util.List;
 import java.util.Optional;
 /**
  *
@@ -19,6 +22,24 @@ public interface UserService {
     Optional<FullUserDTO> updateUser(String email, UpdUserDTO updUser) throws NotFoundServiceException, ConflictServiceException;
     boolean deleteUser(String email) throws NotFoundServiceException;
 
+    /**
+     * Method add carId to user cars
+     * @param userID
+     * @param carID
+     * @return true
+     * @throws NotFoundServiceException - if user not found
+     * @throws ConflictServiceException - if carId already in list
+     */
     boolean addUserCar(String userID, String carID) throws NotFoundServiceException, ConflictServiceException;
-    boolean ifUserCarExist(String userID, String carID) throws NotFoundServiceException;
+    boolean ifUserCarsExist(String userID, String carID) throws NotFoundServiceException;
+    Optional<List<BookedPeriodDto>> getUserBookedCarsPeriods(String userID) throws NotFoundServiceException;
+
+    /**
+     * Method add bookedPeriods to user history
+     * @param userID
+     * @param bookedPeriodEntity
+     * @return true when booked period successfully added
+     * @throws NotFoundServiceException - in case when userID not found
+     */
+    boolean addBookedPeriodToUserHistory(String userID, BookedPeriodEntity bookedPeriodEntity) throws NotFoundServiceException;
 }
