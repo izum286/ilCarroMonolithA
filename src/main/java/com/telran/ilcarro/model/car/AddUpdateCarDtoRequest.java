39d6ee1fa00bbaa56547ea3fa565eab768059408
Anton Konkin <konkin.anton@gmail.com>
2020-01-16 13:45:05 +0200

    json property name refactoring AddUpdateCarDtoRequest

--- src/main/java/com/telran/ilcarro/model/car/AddUpdateCarDtoRequest.java
+++ src/main/java/com/telran/ilcarro/model/car/AddUpdateCarDtoRequest.java
@@ -1,5 +1,6 @@
 package com.telran.ilcarro.model.car;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import lombok.*;
 
@@ -18,6 +19,7 @@ import java.util.List;
 @Builder
 @ApiModel(value = "AddUpdateCarDtoRequest",description = "Request from user to add or update existing car")
 public class AddUpdateCarDtoRequest {
+    @JsonProperty("serial_number")
     private String serialNumber;
     private String make;
     private String model;
@@ -25,18 +27,26 @@ public class AddUpdateCarDtoRequest {
     private String engine;
     private String fuel;
     private String gear;
+    @JsonProperty("wheels_drive")
     private String wheelsDrive;
+    @JsonProperty("horse_power")
     private float horsePower;
     private float torque;
     private int doors;
     private int seats;
+    @JsonProperty("fuel_consumption")
     private float fuelConsumption;
     private List<FeatureDto> features;
+    @JsonProperty("car_class")
     private String carClass;
+    @JsonProperty("price_per_day")
     private PricePerDayDto pricePerDay;
+    @JsonProperty("distance_included")
     private int distanceIncluded;
     private String about;
+    @JsonProperty("pick_up_place")
     private PickUpPlaceDto pickUpPlaceDto;
+    @JsonProperty("image_url")
     private List<String> imageUrl;
 }
 
