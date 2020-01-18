b806fbce0d69c65f2a86f9699d5031dee9697e26
AleksGor224 <47452644+AleksGor224@users.noreply.github.com>
2020-01-18 15:52:08 +0200

    Added TestsConfig configuration bean. Its component scanner for tests.

---
+++ src/main/java/com/telran/ilcarro/config/TestsConfig.java
@@ -0,0 +1,9 @@
+package com.telran.ilcarro.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.telran.ilcarro"})
+public class TestsConfig {
+}
