1a361b32695bee19b5c7fba9ef84b73472b2304c
AleksGor224 <47452644+AleksGor224@users.noreply.github.com>
2020-01-18 15:55:04 +0200

    Added annotations in AuthServiceTests. Changed dependency from some junit to normal junit 4

--- src/test/java/com/telran/ilcarro/ServiceTests/AuthServiceTests.java
+++ src/test/java/com/telran/ilcarro/ServiceTests/AuthServiceTests.java
@@ -1,4 +1,11 @@
 package com.telran.ilcarro.ServiceTests;
 
+import com.telran.ilcarro.config.TestsConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestsConfig.class})
 public class AuthServiceTests {
 }
