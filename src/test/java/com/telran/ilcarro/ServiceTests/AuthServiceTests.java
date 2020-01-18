1ac6de90c6b71c28cf0c032768c1a863bf3d8ab3
AleksGor224 <47452644+AleksGor224@users.noreply.github.com>
2020-01-18 15:57:48 +0200

    Added method setUp, afterTest and firstEmptyTest

--- src/test/java/com/telran/ilcarro/ServiceTests/AuthServiceTests.java
+++ src/test/java/com/telran/ilcarro/ServiceTests/AuthServiceTests.java
@@ -1,6 +1,9 @@
 package com.telran.ilcarro.ServiceTests;
 
 import com.telran.ilcarro.config.TestsConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -8,4 +11,20 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestsConfig.class})
 public class AuthServiceTests {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void afterTest(){
+
+    }
+
+    @Test
+    public void testAuthRegistration(){
+
+    }
+
 }
