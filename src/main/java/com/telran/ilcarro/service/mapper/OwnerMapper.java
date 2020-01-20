6f1355cd73913e63510ce3c2b05fdc01cbc2996a
Anton Konkin <konkin.anton@gmail.com>
2020-01-20 15:58:32 +0200

    mappers upd

--- src/main/java/com/telran/ilcarro/service/mapper/OwnerMapper.java
+++ src/main/java/com/telran/ilcarro/service/mapper/OwnerMapper.java
@@ -4,14 +4,17 @@ import com.telran.ilcarro.model.user.OwnerDtoResponse;
 import com.telran.ilcarro.repository.entity.OwnerEntity;
 import com.telran.ilcarro.repository.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = CommentMapper.class,
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(uses = CommentMapper.class)
 public interface OwnerMapper {
     OwnerMapper INSTANCE = Mappers.getMapper(OwnerMapper.class);
 
+    @Mapping(target = "firstName", source = "firstName", defaultValue = "none")
+    @Mapping(target = "lastName", source = "lastName", defaultValue = "none")
     OwnerDtoResponse map(OwnerEntity entity);
+
     OwnerEntity map(UserEntity entity);
 }
