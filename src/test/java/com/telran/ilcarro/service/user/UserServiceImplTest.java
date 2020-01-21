2c462f868278ea6266c163a3886d23380cf2a8d6
AleksGor224 <47452644+AleksGor224@users.noreply.github.com>
2020-01-21 10:25:02 +0200

    new Refactoring

--- src/test/java/com/telran/ilcarro/service/user/UserServiceImplTest.java
+++ src/test/java/com/telran/ilcarro/service/user/UserServiceImplTest.java
@@ -3,14 +3,32 @@ package com.telran.ilcarro.service.user;
 import com.telran.ilcarro.model.user.FullUserDTO;
 import com.telran.ilcarro.model.user.RegUserDTO;
 import com.telran.ilcarro.model.user.UpdUserDTO;
+import com.telran.ilcarro.repository.UserEntityRepository;
+import com.telran.ilcarro.repository.entity.LocationEntity;
+import com.telran.ilcarro.repository.entity.UserEntity;
+import com.telran.ilcarro.service.exceptions.ConflictServiceException;
+import com.telran.ilcarro.service.mapper.UserMapper;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Optional;
+
+import static java.util.Collections.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
@@ -21,14 +39,19 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserServiceImplTest {
 
-    @MockBean
+    @Autowired
     UserService userService;
+    @MockBean
+    UserEntityRepository userRepository;
 
-    RegUserDTO regUserDTO;
-    UpdUserDTO updUserDTO;
+    private RegUserDTO regUserDTO;
+    private UpdUserDTO updUserDTO;
+    private FullUserDTO fullUserDTO;
+    private FullUserDTO updatedFullUserDTO;
+    private UserEntity userEntity;
 
     @Before
-    public void setUp(){
+    public void init(){
         regUserDTO = RegUserDTO.builder()
                 .firstName("Vasya")
                 .lastName("Pupkin")
@@ -38,59 +61,110 @@ class UserServiceImplTest {
                 .lastName("Beller")
                 .photo("http://someurl.com")
                 .build();
+        fullUserDTO = FullUserDTO.builder()
+                .photo("https://someurl.com/image.jpeg")
+                .booked_car(emptyList())
+                .comments(emptyList())
+                .first_name("Vasya")
+                .history(emptyList())
+                .own_cars(emptyList())
+                .registration_date(LocalDateTime.now().plusHours(23))
+                .second_name("Pupkin")
+                .build();
+        updatedFullUserDTO = FullUserDTO.builder()
+                .photo("http://someurl.com")
+                .booked_car(emptyList())
+                .comments(emptyList())
+                .first_name("Sofa")
+                .history(emptyList())
+                .own_cars(emptyList())
+                .registration_date(LocalDateTime.now().plusHours(23))
+                .second_name("Beller")
+                .build();
+        userEntity = UserEntity.builder()
+                .email("vasyapupkin1234@mail.com")
+                .comments(emptyList())
+                .firstName("Vasya")
+                .history(emptyList())
+                .lastName("Pupkin")
+                .photo("https://someurl.com/image.jpeg")
+                .registrationDate(fullUserDTO.getRegistration_date())
+                .driverLicense("5r1325136135")
+                .isDeleted(false)
+                .location(LocationEntity.builder()
+                        .zip(12124)
+                        .street("strit")
+                        .state("staaate")
+                        .lon("4323.564345")
+                        .lat("5345.323423")
+                        .isVehicle(false)
+                        .country("cauntri")
+                        .city("ceeety")
+                        .build())
+                .ownCars(emptyList())
+                .phone("452346236235")
+                .build();
     }
 
     @Test
     void addUser() {
-        assertTrue(userService.addUser("vasyapupkin1234@mail.com",regUserDTO).isPresent());
+        //Annotation @Before not worked. Used method init for initialization of all entities and dtos, because we dont use DB
+        init();
+        Mockito.doReturn(userEntity).when(userRepository).save(ArgumentMatchers.any());
+        Mockito.doReturn(Optional.of(userEntity)).when(userRepository).findById(ArgumentMatchers.anyString());
+        Optional<FullUserDTO> check = userService.addUser("vasyapupkin1234@mail.com",regUserDTO);
+        check.ifPresent(userDTO -> assertNotNull(userDTO.getRegistration_date()));
+        Mockito.verify(userRepository,Mockito.times(1)).save(ArgumentMatchers.any(UserEntity.class));
+        Mockito.verify(userRepository,Mockito.times(3)).findById("vasyapupkin1234@mail.com");
+    }
+
+    @Test
+    void addUserWithNullEmail() {
+        init();
+
+    }
+
+    @Test
+    void addUserIfExists(){
+        init();
+
     }
 
     @Test
     void getUser() {
-        userService.addUser("vasyapupkin1234@mail.com",regUserDTO);
-        if(userService.getUser("vasyapupkin1234@mail.com").isPresent()){
-            assertEquals(userService.getUser("vasyapupkin1234@mail.com").get().getFirst_name(),regUserDTO.getFirstName());
-        }
+        init();
+
     }
 
     @Test
     void updateUser() {
-        userService.addUser("vasyapupkin1234@mail.com",regUserDTO);
-        FullUserDTO tmp = userService.getUser("vasyapupkin1234@mail.com").orElseThrow();
-        FullUserDTO tmp2 = userService.updateUser("vasyapupkin1234@mail.com",updUserDTO).orElseThrow();
-        assertNotEquals(tmp.getFirst_name(),tmp2.getFirst_name());
-        assertNotEquals(tmp.getSecond_name(),tmp2.getSecond_name());
-        assertNotEquals(tmp.getPhoto(),tmp2.getPhoto());
-        assertEquals(tmp.getComments(),tmp2.getComments());
-        assertEquals(tmp.getHistory(),tmp2.getHistory());
-        assertEquals(tmp.getRegistration_date(),tmp2.getRegistration_date());
-        assertEquals(tmp.getBooked_car(),tmp2.getBooked_car());
-        assertEquals(tmp.getOwn_cars(),tmp2.getOwn_cars());
+        init();
+
     }
 
     @Test
     void deleteUser() {
-        userService.addUser("vasyapupkin1234@mail.com",regUserDTO);
-        assertTrue(userService.deleteUser("vasyapupkin1234@mail.com"));
-        userService.addUser("vasyapupkin1234@mail.com",regUserDTO);
-        assertTrue(userService.deleteUser("vasyapupkin1234@mail.com"));
-        userService.addUser("vasyapupkin1234@mail.com",regUserDTO);
-        assertTrue(userService.deleteUser("vasyapupkin1234@mail.com"));
+        init();
+
     }
 
     @Test
     void addUserCar() {
+        init();
     }
 
     @Test
     void ifUserCarsExist() {
+        init();
     }
 
     @Test
     void getUserBookedCarsPeriods() {
+        init();
     }
 
     @Test
     void addBookedPeriodToUserHistory() {
+        init();
     }
 }
