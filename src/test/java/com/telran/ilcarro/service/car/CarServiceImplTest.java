68d93f6bf892e0985639d7890ec93ebd16da8a32
AleksGor <AleksGor>
2020-01-22 22:06:51 +0200

    Added CarServiceImplTest

---
+++ src/test/java/com/telran/ilcarro/service/car/CarServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.telran.ilcarro.service.car;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceImplTest {
+
+    @Test
+    void addCar() {
+    }
+
+    @Test
+    void updateCar() {
+    }
+
+    @Test
+    void deleteCar() {
+    }
+
+    @Test
+    void getCarByIdForUsers() {
+    }
+
+    @Test
+    void getCarByIdForOwner() {
+    }
+
+    @Test
+    void ownerGetCars() {
+    }
+
+    @Test
+    void getBookedPeriodsByCarId() {
+    }
+
+    @Test
+    void makeReservation() {
+    }
+
+    @Test
+    void getThreeBestCars() {
+    }
+
+    @Test
+    void getCarStatById() {
+    }
+}
