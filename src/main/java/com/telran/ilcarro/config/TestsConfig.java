567c61b4e956e8a8415eeacc562477cc5b5a5a36
AleksGor <AleksGor>
2020-01-22 11:29:41 +0200

    Removed not used config. Its for JUnitRunner

--- src/main/java/com/telran/ilcarro/config/TestsConfig.java
+++
@@ -1,9 +0,0 @@
-package com.telran.ilcarro.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.telran.ilcarro"})
-public class TestsConfig {
-}
