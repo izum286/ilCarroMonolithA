0957bd1b9036da88e2541d5732f9dd57f0ad4529
AleksGor <AleksGor>
2020-01-25 19:17:44 +0200

    Added FilterServiceImplTest. Added annotations

---
+++ src/test/java/com/telran/ilcarro/service/filter/FilterServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.telran.ilcarro.service.filter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class FilterServiceImplTest {
+
+    @Test
+    void addFilter() {
+    }
+
+    @Test
+    void provideFilter() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void mergeNodes() {
+    }
+
+    @Test
+    void findNextIndx() {
+    }
+
+    @Test
+    void deleteFilters() {
+    }
+}
