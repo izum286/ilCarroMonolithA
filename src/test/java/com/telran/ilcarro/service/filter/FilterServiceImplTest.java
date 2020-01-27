079b23c2e5be1810be254ff11efb44bb7794580b
AleksGor <AleksGor>
2020-01-27 23:26:56 +0200

    Added fields and init method

--- src/test/java/com/telran/ilcarro/service/filter/FilterServiceImplTest.java
+++ src/test/java/com/telran/ilcarro/service/filter/FilterServiceImplTest.java
@@ -1,8 +1,15 @@
 package com.telran.ilcarro.service.filter;
 
+import com.telran.ilcarro.model.car.AddUpdateCarDtoRequest;
+import com.telran.ilcarro.model.filter.FilterDTO;
+import com.telran.ilcarro.repository.FilterRepository;
+import com.telran.ilcarro.repository.entity.FilterNodeEntity;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +17,17 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class FilterServiceImplTest {
+
+    @Autowired
+    FilterService filterService;
+
+    @MockBean
+    FilterRepository filterRepository;
+
+    AddUpdateCarDtoRequest addUpdateCarDtoRequest;
+    FilterDTO filterDTO;
+    FilterNodeEntity filterNodeEntity;
+
 
     @Test
     void addFilter() {
@@ -33,5 +51,10 @@ class FilterServiceImplTest {
 
     @Test
     void deleteFilters() {
+    }
+
+    @Before
+    void init(){
+
     }
 }
