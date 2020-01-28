17222a08856d74238eab4db2c432f1a6523242d1
AleksGor <AleksGor>
2020-01-28 21:46:32 +0200

    Added SearchServiceImplTest. Added annotations and method init

---
+++ src/test/java/com/telran/ilcarro/service/search/SearchServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.telran.ilcarro.service.search;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SearchServiceImplTest {
+
+    @Test
+    void cityDatesPriceSortByPrice() {
+    }
+
+    @Test
+    void geoAndRadius() {
+    }
+
+    @Test
+    void byFilter() {
+    }
+
+    @Test
+    void searchAllSortByPrice() {
+    }
+
+    @Before
+    public void init(){
+
+    }
+}
